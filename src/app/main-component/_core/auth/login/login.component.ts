0e0b7e32cee0e9f110d2cbf8732bbecf6a8d4fff
KaranDuggal <duggalkaran400@gmail.com>
2022-12-29 18:24:49 +0530

    login form email validation add

--- src/app/main-component/_core/auth/login/login.component.ts
+++ src/app/main-component/_core/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormBuilder, FormArray} from "@angular/forms";
+import { FormControl, Validators, FormBuilder, FormGroup} from "@angular/forms";
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,35 @@ import { FormControl, FormGroup, Validators, FormBuilder, FormArray} from "@angu
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  loginFrom: any;
+  constructor(
+    private formBuilder: FormBuilder,
+    // private apiService: ApiService,
+    // private alertService: AlertService,
+    // private router: Router,
+    // private activatedRoute: ActivatedRoute,
+  ) { 
+    this.checkToken();
+    this.form();
+  }
+  form(){
+    this.loginFrom = this.formBuilder.group({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,]),
+      role: new FormControl('1'),
+    })
+  }
+  formSubmit(){
+    if(!this.loginFrom.valid) return;
+    console.log('this.loginFrom', this.loginFrom.value)
+    // this.apiService.callAPI('post', this.loginFrom.value, `admin/auth/login`,true).subscribe((data:any)=>{
+    //   localStorage.setItem('token', data.token);
+    //   this.router.navigate(['/tag']);
+    // })
+  }
+  checkToken(){
+    if(localStorage.getItem('token')){
+      // this.router.navigate(['/test']);
+    }
+  }
 }
